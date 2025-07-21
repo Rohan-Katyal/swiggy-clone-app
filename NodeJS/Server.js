6cec1edcfad46ffadae4148e83ff5b336e8a1cc1
Rohan Katyal <rohankatyal74@gmail.com>
2025-07-22 00:26:19 +0530

    changes

--- NodeJS/Server.js
+++ NodeJS/Server.js
@@ -17,7 +17,7 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0/0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
